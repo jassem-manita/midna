f5a5a3be68926a20f4539d91f6b39df465bb6063
jassem-manita <jasemmanita00@gmail.com>
2025-07-09 22:13:28 +0100

    Clean stable version before restructuring

--- setup.py
+++ setup.py
@@ -1,25 +1,41 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Installation/Setup",
-    ],
-    python_requires=">=3.6",
-    entry_points={
-        "console_scripts": [
-            "zap=zap:main",
+    setup(
+        name="zap",
+        version="0.1.0",
+        description="A simple and friendly pip requirements installer with pre-install checks",
+        long_description=open('README.md').read(),
+        long_description_content_type="text/markdown",
+        author="Jassem",
+        author_email="jasemmanita00@gmail.com",
+        url="https://github.com/jassem-manita/zap",
+        py_modules=["zap"],
+        install_requires=[],
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: System :: Installation/Setup",
         ],
-    },
-    keywords="pip, package, installer, requirements, dry-run",
-    project_urls={
-        "Bug Reports": "https://github.com/jassem/zap/issues",
-        "Source": "https://github.com/jassem/zap",
-    },
-)
+        python_requires=">=3.6",
+        entry_points={
+            "console_scripts": [
+                "zap=zap:main",
+            ],
+        },
+        keywords="pip, package, installer, requirements, dry-run",
+        project_urls={
+            "Bug Reports": "https://github.com/jassem-manita/zap/issues",
+            "Source": "https://github.com/jassem-manita/zap",
+        },
+    )
